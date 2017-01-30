22af91160d072fa57b36416cc7bbd40dd00f846d
warrior <jcayetano408@gmail.com>
2017-01-30 22:17:19 +0000

    made flag

--- script.js
+++ script.js
@@ -1,1 +1,61 @@
 //Write your function calls below this line
+setColor(0, 0, "blue");
+setColor(1, 0, "blue");
+setColor(2, 0, "blue");
+setColor(3, 0, "blue");
+setColor(4, 0, "blue");
+setColor(5, 0, "blue");
+
+setColor(0, 1, "blue");
+setColor(1, 1, "blue");
+setColor(2, 1, "blue");
+setColor(3, 1, "blue");
+setColor(4, 1, "blue");
+setColor(5, 1, "blue");
+
+setColor(0, 2, "blue");
+setColor(1, 2, "blue");
+setColor(2, 2, "blue");
+setColor(3, 2, "blue");
+setColor(4, 2, "blue");
+setColor(5, 2, "blue");
+
+setColor(0, 3, "white");
+setColor(1, 3, "white");
+setColor(2, 3, "white");
+setColor(3, 3, "white");
+
+
+setColor(0, 6, "red");
+setColor(1, 6, "red");
+setColor(2, 6, "red");
+setColor(3, 6, "red");
+setColor(4, 6, "red");
+setColor(5, 6, "red");
+
+setColor(0, 7, "red");
+setColor(1, 7, "red");
+setColor(2, 7, "red");
+setColor(3, 7, "red");
+setColor(4, 7, "red");
+setColor(5, 7, "red");
+
+setColor(0, 8, "red");
+setColor(1, 8, "red");
+setColor(2, 8, "red");
+setColor(3, 8, "red");
+setColor(4, 8, "red");
+setColor(5, 8, "red");
+
+
+
+
+
+
+
+
+
+
+
+
+
